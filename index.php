3cff49e6568356b0e9bc915a85946e7fb1a4d314
Cibirsk <stlv@gmx.com>
2022-03-09 07:05:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,20 @@
 
 class User
 {
+    public string $name = 'Jean';
 
+    public function greeting()
+    {
+        return "Bonjour $this->name";
+    }
 }
 
 $user = new User();
+echo $user->name . '<br>';
+
+$user->name = 'Bob';
+echo $user->name . '<br>';
+
+echo $user->greeting() . '<br>';
 
 var_dump($user);
