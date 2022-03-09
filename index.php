1153ddda74d18510b4432a81f6a765c1c775079e
Cibirsk <stlv@gmx.com>
2022-03-09 18:05:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,31 +2,17 @@
 
 class User
 {
-    public string $name = 'Jean';
+    public $name ;
 
-    public function greeting()
+    public function __construct()
     {
-        return "Bonjour $this->name";
+        echo 'construct call' . '<br>';
     }
 }
 
 $user = new User();
-echo $user->name . '<br>';
-
 $user->name = 'Bob';
 echo $user->name . '<br>';
 
-$user2 = $user;
-echo $user2->name . '<br>';
-
-$user->name = 'BobY';
-echo $user2->name . '<br>';
 // echo $user->greeting() . '<br>';
 // var_dump($user);
-
-$user3 = 'Mike';
-echo $user3 . '<br>';
-
-$user4 = $user3;
-$user3 = 'MikY';
-echo $user4 . '<br>';
