ef8efb490611cfa96e6f760d31469d5dafd1126d
Cibirsk <stlv@gmx.com>
2022-03-14 18:13:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,20 @@
 <pre>
 <?php
 
-class User {
+class Math{
+    public static float $pi = 3.14;
 
-  private string $nomComplet;
-  
-  function __construct(private string $prenom, private string $nom) {
-    $this-> nomComplet = $prenom . ' ' . $nom;
-  }
-
-  function __isset($prop) {
-    return isset($this->$prop);
-  }
-
-  function __get(string $prop) {
-    if (isset($this->$prop)) {
-      return $this->$prop;
-    } else {
-      echo "La propriété n'existe pas !";
+    static function getPi(){
+        return self::$pi; //remplace this->
     }
-  } 
-
-  function __set(string $prop, string $val) {
-    $this->$prop = $val;
-    if ($prop === 'prenom' || $prop === 'nom') {
-      $this->nomComplet = $this->prenom . ' ' . $this->nom;
-    }
-  }
 }
 
-$user1 = new User('Jean', 'Dupont');
-echo isset($user1->nom), '<br>'; // 1
+echo Math::$pi;      //remplace ->
+echo Math::getPi();
+
+$math = new Math();
+echo $math::$pi;
+echo $math::getPi();
+
 ?>
 </pre>
