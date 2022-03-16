91c89437f4a549aec3f8bcd92f80eea30b278ce9
Cibirsk <stlv@gmx.com>
2022-03-16 18:17:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,39 @@
 <pre>
 <?php
 
-abstract class Greeting 
-{
-    function setFirstName($value)
-    {
-        $this->firstName = $value;
-    }
+abstract class User {
+  public function __construct(public string $nom) {}
 
-    function getFirstName() 
-    {
-        return $this->firstName;
-    }
-
-    function setLastName($value)
-    {
-        $this->lastName = $value;
-    }
-
-    function getLastName() 
-    {
-        return $this->lastName;
-    }
-    
-    abstract function greeting();
+  abstract public function greeting() : string;
 }
 
-class User extends Greeting
-{
-
+class Admin extends User {
+  public function greeting() : string {
+    return "Je suis un admin et je m'appelle $this->nom !";
+  }
 }
 
-class Admin extends Greeting
-{
-
+class Moderator extends User {
+  public function greeting() : string {
+    return "Je suis un modérateur et je m'appelle $this->nom !";
+  }
 }
 
+class Registered extends User {
+  public function greeting() : string {
+    return "Je suis un utilisateur et je m'appelle $this->nom !";
+  }
+}
+
+$admin = new Admin("Jean"); // Je suis un admin et je m'appelle Jean !
+echo $admin->greeting();
+echo "<br>";
+
+$modo = new Moderator("Paul"); // Je suis un modérateur et je m'appelle Paul !
+echo $modo->greeting();
+echo "<br>";
+
+$user = new Registered("Bob"); // Je suis un utilisateur et je m'appelle Bob !
+echo $user->greeting();
 ?>
 </pre>
