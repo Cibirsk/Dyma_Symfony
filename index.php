194dad85dbf8acaa17cc7d8c821eb477f4fc9b8d
Cibirsk <stlv@gmx.com>
2022-03-18 17:03:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,29 +5,31 @@ class Wallet
 {
   function __construct(public int $amount)
   {
-    
   }
 }
 
 class User
 {
-  private string $fullName;
-  private bool $isAdmin;
   public Wallet $wallet;
-
-  function setFullName(string $fullName): void
-  {
-    $this->fullName = $fullName;
-  }
 
   function getWallet(): Wallet
   {
     return $this->wallet;
   }
 
+  function setWallet(Wallet $wallet)
+  {
+    $this->wallet = $wallet;
+  }
+
 }
 
+$user = new User();
+$theWallet = new Wallet(200);
+$user->setWallet($theWallet);
 
+echo $user->wallet->amount;
+//echo $user->getWallet();
 
 ?>
 </pre>
