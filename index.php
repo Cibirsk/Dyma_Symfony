9e8a004af769d323eb3d6db0bea64921b1be4ca4
Cibirsk <stlv@gmx.com>
2022-03-18 09:16:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,39 +1,33 @@
 <pre>
 <?php
 
-abstract class User {
-  public function __construct(public string $nom) {}
-
-  abstract public function greeting() : string;
-}
-
-class Admin extends User {
-  public function greeting() : string {
-    return "Je suis un admin et je m'appelle $this->nom !";
+class Wallet
+{
+  function __construct(public int $amount)
+  {
+    
   }
 }
 
-class Moderator extends User {
-  public function greeting() : string {
-    return "Je suis un modérateur et je m'appelle $this->nom !";
+class User
+{
+  private string $fullName;
+  private bool $isAdmin;
+  public Wallet $wallet;
+
+  function setFullName(string $fullName): void
+  {
+    $this->fullName = $fullName;
   }
+
+  function getWallet(): Wallet
+  {
+    return $this->wallet;
+  }
+
 }
 
-class Registered extends User {
-  public function greeting() : string {
-    return "Je suis un utilisateur et je m'appelle $this->nom !";
-  }
-}
 
-$admin = new Admin("Jean"); // Je suis un admin et je m'appelle Jean !
-echo $admin->greeting();
-echo "<br>";
 
-$modo = new Moderator("Paul"); // Je suis un modérateur et je m'appelle Paul !
-echo $modo->greeting();
-echo "<br>";
-
-$user = new Registered("Bob"); // Je suis un utilisateur et je m'appelle Bob !
-echo $user->greeting();
 ?>
 </pre>
