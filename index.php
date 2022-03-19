15b771fef0df8598d80110f17cbb045b29861a82
Cibirsk <stlv@gmx.com>
2022-03-19 10:13:24 +0100

    none

--- index.php
+++ index.php
@@ -1,35 +1,15 @@
 <pre>
 <?php
 
-class Wallet
+function func()
 {
-  function __construct(public int $amount)
-  {
-  }
-}
-
-class User
-{
-  public Wallet $wallet;
-
-  function getWallet(): Wallet
-  {
-    return $this->wallet;
-  }
-
-  function setWallet(Wallet $wallet)
-  {
-    $this->wallet = $wallet;
-  }
 
 }
 
-$user = new User();
-$theWallet = new Wallet(200);
-$user->setWallet($theWallet);
+function func()
+{
 
-echo $user->wallet->amount;
-//echo $user->getWallet();
+}
 
 ?>
 </pre>
