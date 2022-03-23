63e316f39182d1d20f855b856dab33d8f3a07635
Cibirsk <stlv@gmx.com>
2022-03-23 09:15:22 +0100

    none

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <pre>
 <?php
 
-require __DIR__ . "/lib/models/User.php";
-require __DIR__ . "/lib/models/Email.php";
-use Dyma\models\{User, Admin, Email};  
-//antislash pour indiquer le namespace et {} pour utiliser 3 classes même dans 2 fichiers différents
-
-$user = new User(); 
-$email = new Email();
-$admin = new Admin();
 
 ?>
 </pre>
